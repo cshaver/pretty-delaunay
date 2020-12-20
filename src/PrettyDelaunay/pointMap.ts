d9be490d08e8e34452fc1ef7e08641fed008086e
Cristina Shaver <cristina.shaver@gmail.com>
2020-12-20 14:57:41 -0800

    convert pointMap to TS

--- src/PrettyDelaunay/pointMap.ts
+++ src/PrettyDelaunay/pointMap.ts
@@ -5,32 +5,30 @@ import Point from './point';
  * @class
  */
 export default class PointMap {
-  constructor() {
-    this._map = {};
-  }
+  private _map: { [point: string]: boolean } = {};
 
   // adds point to map
-  add(point) {
+  add(point: Point): void {
     this._map[point.toString()] = true;
   }
 
   // adds x, y coord to map
-  addCoord(x, y) {
+  addCoord(x: number, y: number): void {
     this.add(new Point(x, y));
   }
 
   // removes point from map
-  remove(point) {
+  remove(point: Point): void {
     this._map[point.toString()] = false;
   }
 
   // removes x, y coord from map
-  removeCoord(x, y) {
+  removeCoord(x: number, y: number): void {
     this.remove(new Point(x, y));
   }
 
   // clears the map
-  clear() {
+  clear(): void {
     this._map = {};
   }
 
@@ -39,7 +37,7 @@ export default class PointMap {
    * added to map already
    *  @returns {Boolean}
    */
-  exists(point) {
+  exists(point: Point): boolean {
     return this._map[point.toString()] ? true : false;
   }
 }
